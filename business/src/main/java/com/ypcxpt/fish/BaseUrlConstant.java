692fe1ac649753a7952bdb024e2dd144feca7cc3
Lenny_Xu <820248206@qq.com>
2020-05-28 18:02:15 +0800

    域名修改；key升级

--- business/src/main/java/com/ypcxpt/fish/BaseUrlConstant.java
+++ business/src/main/java/com/ypcxpt/fish/BaseUrlConstant.java
@@ -9,10 +9,12 @@ public class BaseUrlConstant {
     /**
      * 后台接口
      */
-    public static final String BASE_URL = "https://fish.ypcxpt.com/";
+//    public static final String BASE_URL = "https://fish.ypcxpt.com/";
+    public static final String BASE_URL = "https://fish.smartbreed.cn/";
 
     /**
      * webSocket
      */
-    public static final String WEBSOCKET_URI = "wss://fish.ypcxpt.com?token=" + AppData.token();
+//    public static final String WEBSOCKET_URI = "wss://fish.ypcxpt.com?token=" + AppData.token();
+    public static final String WEBSOCKET_URI = "wss://fish.smartbreed.cn?token=" + AppData.token();
 }
